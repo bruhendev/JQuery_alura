bac80ee3dd6747eb911f27944462cb67200c3ddb
Bruno <brunohenrique242@gmail.com>
2021-09-28 16:06:09 -0300

    Reiniciando o jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,51 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-campo.on("input", function(){
-    var conteudo = campo.val();
-    var contCaracteres = conteudo.length;
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-caracteres").text(contCaracteres);
-    $("#contador-palavras").text(qtdPalavras);
+$(function () {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("click", function(){
-    var cronometroId = setInterval(function(){
-        tempoRestante--
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1){
-            campo.attr("disabled", true);
-            clearInterval(cronometroId);
-        }
-    }, 1000);
-});
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function () {
+        var conteudo = campo.val();
+        var contCaracteres = conteudo.length;
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-caracteres").text(contCaracteres);
+        $("#contador-palavras").text(qtdPalavras);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("click", function () {
+        var cronometroId = setInterval(function () {
+            tempoRestante--
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
+
 
